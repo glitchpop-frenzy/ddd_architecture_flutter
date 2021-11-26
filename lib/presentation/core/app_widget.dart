9246aa246e0317cbc7913cf3ebafeb5b47f363d0
Saksham Gupta <sakshamgupta391@gmail.com>
2021-11-27 04:24:46 +0530

    added auto_router and set up for navigation (missing Splash Screen)

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,3 +1,8 @@
+import 'package:ddd_architecture_flutter/application/auth/auth/bloc/auth_bloc.dart';
+import 'package:ddd_architecture_flutter/presentation/injection.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auto_route/auto_route.dart';
+
 import '../sign_in/sign_in_page.dart';
 import 'package:flutter/material.dart';
 
@@ -6,21 +11,28 @@ class AppWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notes',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.green[800],
-        colorScheme: ThemeData().colorScheme.copyWith(
-              secondary: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp(
+          title: 'Notes',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.light().copyWith(
+            primaryColor: Colors.green[800],
+            colorScheme: ThemeData().colorScheme.copyWith(
+                  secondary: Colors.blue,
+                ),
+            inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
           ),
-        ),
-      ),
-      home: const SignInPage(),
+          builder: ExtendedNavigator(router: Router())),
     );
     ;
   }
---
+++ lib/presentation/route/router.dart
@@ -0,0 +1,9 @@
+// This class will contain the definitionsof Routes in our app
+
+import 'package:auto_route/auto_route.dart';
+import 'package:ddd_architecture_flutter/presentation/sign_in/sign_in_page.dart';
+
+@MaterialAutoRouter()
+class $Router {
+  late SignInPage signInPage;
+}
